4f6c44d11aae6ce999264c2e2efbabc6b9df7636
Brosnan Yuen <130252102+BrosnanYuen@users.noreply.github.com>
2026-06-13 22:56:37 -0700

    Add files via upload

---
+++ symbolic_math_verify/__init__.py
@@ -0,0 +1,19 @@
+"""Public package exports for symbolic math verification."""
+
+from .check_calculation import is_calculation_correct
+from .check_subscript_substitution import is_subscript_substitution_correct
+from .check_substitution import is_substitution_correct
+from .check_util import is_equation_equal
+from .extract_variables import extract_variables
+from .verify_yaml import verify_yaml_file
+
+__all__ = [
+    "extract_variables",
+    "is_calculation_correct",
+    "is_equation_equal",
+    "is_subscript_substitution_correct",
+    "is_substitution_correct",
+    "verify_yaml_file",
+]
+
+__version__ = "0.1.0"
