9fd58ffe500d2fd8afcdcffe8a6722bf7b042e76
Brosnan Yuen <brosnanyuen@gmail.com>
2026-07-10 20:50:40 -0700

    save

--- src/symbolic_math_mcp/__init__.py
+++
@@ -1,9 +0,0 @@
-"""symbolic_math_mcp package."""
-
-from .config import ServerConfig, load_config
-from .server import create_mcp_server
-from .service import SymbolicMathService
-
-__all__ = ["ServerConfig", "SymbolicMathService", "create_mcp_server", "load_config"]
-
-__version__ = "0.1.0"
